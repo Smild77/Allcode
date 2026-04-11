e7ad7e102111546045264505762fbb3aabbae4b9
Smild77 <ariyatuchzaza@gmail.com>
2026-04-11 12:54:59 +0700

    'test'

--- Automation Testing/playwright.config.ts
+++ Automation Testing/playwright.config.ts
@@ -30,6 +30,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    userAgent: 'QA-Candidate-Mild-Arithat-Testing-For-Persec',
   },
 
   /* Configure projects for major browsers */
